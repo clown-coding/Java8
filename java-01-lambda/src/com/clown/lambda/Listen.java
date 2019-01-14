6001c852bbd4e2d2c01e4f314bbc47845194b7eb
Richard <ljy8953898@gmail.com>
2019-01-14 22:03:02 +0800

    Initial commit

---
+++ java-01-lambda/src/com/clown/lambda/Listen.java
@@ -0,0 +1,12 @@
+package com.clown.lambda;
+
+/**
+ * @author: Richard·Ackerman
+ * @create: 2019/1/14
+ * @description: listen
+ **/
+public class Listen {
+    public static void main(String[] args){
+        System.out.println("clown is laughing at you...");
+    }
+}
